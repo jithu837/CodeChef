2b5845919bedaef618bb05cfacfed89428f6c4fb
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:27:14 +0530

    Add files via upload

---
+++ TheIdeasOfMarch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+class TheIdeasOfMarch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==15)
+		{
+		    System.out.println("Yes");
+		}
+		else
+		{
+		    System.out.println("No");
+		}
+
+	}
+}
