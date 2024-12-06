6b8fa9a5564e74a181a8aabe709f5a7e5ea87813
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:27:03 +0530

    Add files via upload

---
+++ ThalaForAReason.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class ThalaForAReason
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==7)
+		{
+		    System.out.println("THALA");
+		}
+		else{
+		    System.out.println("SADGE");
+		}
+
+	}
+}
