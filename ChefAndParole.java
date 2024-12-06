74579ea21533e86673537c6a7eb80f1af2ddd786
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:26:26 +0530

    Add files via upload

---
+++ ChefAndParole.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class ChefAndParole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>=7)
+		{
+		    System.out.println("Yes");
+		}
+		else 
+		{
+		    System.out.println("No");
+		}
+
+	
+
+
+	
+	}
+}
