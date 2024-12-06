9055aca7f71d03c487d3636a321c9567a490d546
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:26:37 +0530

    Add files via upload

---
+++ ChristmasGreetings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class ChristmasGreetings
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int d=sc.nextInt();
+        if(d==25)
+        {
+            System.out.println("CHRISTMAS");
+        
+        }else{
+            System.out.println("ORDINARY");
+        }
+	}
+}
