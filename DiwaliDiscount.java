39aec8b2b94e38bf8cbff427b0ba84abfe8bdc7b
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:26:51 +0530

    Add files via upload

---
+++ DiwaliDiscount.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+class DiwaliDiscount
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=Math.max(0,a-b);
+		System.out.println(c);
+
+	}
+}
