a6ca0509fb0779a18ea5fadc442a570122a7b333
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-06 11:26:15 +0530

    Add files via upload

---
+++ AiIsComing.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class AiIsComing
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=60)
+		{System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
