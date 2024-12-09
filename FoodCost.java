9eaf06deba3abdc9d84c3d4aaa8902e6d5a539ca
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-09 15:55:44 +0530

    Add files via upload

---
+++ FoodCost.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class FoodCost
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a<=1200)
+		{
+		    System.out.println(a*6+b);
+		}
+
+	}
+}
