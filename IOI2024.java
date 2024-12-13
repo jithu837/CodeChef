3904b1c9b027491ef07113c4de635c98705c5ae7
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-13 15:28:18 +0530

    Create IOI2024.java

---
+++ IOI2024.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class IOI2024
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();   
+        if (X >= 1 && X <= 8) {
+            System.out.println("YES");  
+        } else {
+            System.out.println("NO");    
+        }
+    }
+}
+
+
+
