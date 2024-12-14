23d46c4906e4fa577a2b8baa37420d8e4e76dc54
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-14 15:28:39 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class AddTwoNumbers
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		int res=(a+b);
+    		System.out.println(res);
+		}
+		
+	}
+}
