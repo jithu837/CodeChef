797c6e24589c2d99fdd0dda8627b0842624c5293
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-14 15:26:08 +0530

    Create ClearDay.java

---
+++ ClearDay.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ClearDay
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int res=7-(a+b);
+		System.out.println(res);
+
+	}
+}
