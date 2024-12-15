329c85f672ee159b1422e1dd2ab811094c748e75
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-15 11:22:24 +0530

    Create CreditScore.java

---
+++ CreditScore.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CreditScore
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=750) System.out.println("Yes");
+		else System.out.println("No");
+
+	}
+}
