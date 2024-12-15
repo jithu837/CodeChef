5aeb4348376c27230d2c37032e55662b841dc780
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-15 11:10:38 +0530

    Create AirQualityIndex.java

---
+++ AirQualityIndex.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AirQualityIndex
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x<100) System.out.println("YES");
+		else System.out.println("NO");
+		
+
+	}
+}
