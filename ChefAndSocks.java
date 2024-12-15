6c7bd5256c49fe4c808e03e43c04d5c7c48561fb
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-15 11:20:20 +0530

    Create ChefAndSocks.java

---
+++ ChefAndSocks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChefAndSocks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		System.out.println(((b+c)>a)?"YES":"NO");
+
+	}
+}
