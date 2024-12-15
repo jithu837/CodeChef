3176c64d0fb0d1fa61be1157dee44e8a839f16e2
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-15 11:17:35 +0530

    Create AlgomaniacFinals.java

---
+++ AlgomaniacFinals.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AlgomaniacFinals
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x==17) System.out.println("YAY");
+		else System.out.println("NO");
+
+	}
+}
