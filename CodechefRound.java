976d750de1b52c1cdf2e2d38f110724514b7cfe8
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-23 23:10:51 +0530

    Create CodechefRound.java

---
+++ CodechefRound.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class CodechefRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read the day of the week
+        int n = sc.nextInt();
+
+        // Check if today is Wednesday
+        if (n == 4) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
