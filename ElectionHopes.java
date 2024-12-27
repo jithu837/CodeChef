455f4d7b84778b578362d4f7d7bb9bec438d34c2
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-27 23:17:58 +0530

    Create ElectionHopes.java

---
+++ ElectionHopes.java
@@ -0,0 +1,15 @@
+mport java.util.*;
+class ElectionHopes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int Y = scanner.nextInt();
+        if (X >= 2 * Y) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        scanner.close();
+    }
+}
