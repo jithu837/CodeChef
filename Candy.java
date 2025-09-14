de478f89f31a34c4d4130223036b1b4e7aa1574d
Jithendra Kumar <jithendrasolanki56@gmail.com>
2025-09-15 00:33:15 +0530

    Create Candy.java

---
+++ Candy.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();  // number of test cases
+        
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();  // number of candies
+            
+            if (N % 3 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+	}
+}
