f786a31cfcad5703427d19be7eb5c5fe6649ee48
Jithendra Kumar <jithendrasolanki56@gmail.com>
2025-09-21 16:55:50 +0530

    Create ChartPaper.java

---
+++ ChartPaper.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ChartPaper
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int N=sc.nextInt();
+		    int K=sc.nextInt();
+		    int chartpaper=(N / K) * (N / K);
+		   
+		        System.out.println(chartpaper);
+		    
+		}
+
+	}
+}
