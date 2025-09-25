bc3e5c2bc4eb2cc5cd4abbd3a3b74ca908a2e3ff
Jithendra Kumar <jithendrasolanki56@gmail.com>
2025-09-25 23:25:18 +0530

    Create ExerciseAndRest.java

---
+++ ExerciseAndRest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ExerciseAndRest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int Exercise=3*N;
+		System.out.println(Exercise);
+
+	}
+}
