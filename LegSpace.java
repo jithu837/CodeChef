55c4c2967484c69118c9ba7818010448350d8ff6
Jithendra Kumar <jithendrasolanki56@gmail.com>
2025-10-05 23:10:00 +0530

    Create LegSpace.java

---
+++ LegSpace.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LegSpace
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	    Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    int M=sc.nextInt();
+	    if(N< M) System.out.println("Yes");
+	    else System.out.println("No");
+
+	}
+}
