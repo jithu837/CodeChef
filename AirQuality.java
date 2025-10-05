9682af24b5c21081eb52357ee76dd097caa3bbfb
Jithendra Kumar <jithendrasolanki56@gmail.com>
2025-10-05 23:14:49 +0530

    Create AirQuality.java

---
+++ AirQuality.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AirQuality
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+         Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    
+	    if(N< 100) System.out.println("Yes");
+	    else System.out.println("No");
+	}
+}
