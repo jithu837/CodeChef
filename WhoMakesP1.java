dca026d36e17772db71f18da53d72eee5804582b
G Jithendra Kumar <jithendrasolanki56@gmail.com>
2026-02-06 23:38:42 +0530

    Add WhoMakesP1.java for input comparison

---
+++ WhoMakesP1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WhoMakesP1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		if(A<=B) System.out.println("Tyro");
+		else System.out.println("Dom");
+
+	}
+}
